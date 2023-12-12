2306736612f58824a7f41edd8856341ab2c14ad5
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2023-12-12 00:32:36 -0800

    fixed more errors

--- app/Q2/page.tsx
+++ app/Q2/page.tsx
@@ -2,8 +2,7 @@ export default function Q2() {
   const content = `Next JS is a framework that holds React on its base, while React JS is an open-source JavaScript library, developed and maintained by Facebook. Next JS is used to create web applications and performs server-side rendering, whereas React JS focuses on rendering towards the DOM.
     
     Next.js has additional tooling and supports out of the box support for Typescript, hot reloading without component state loss,bunding, route pre-fetching, hybrid rendering.
-     
-    Next JS offers server-side rendering (SSR), whereas Create React App offers client-side rendering, improving the application performance.
+
 
     Next.js is a framework, used to build UI and pages for the web app with React library, while React.js as a library is a part of a framework, the UI components part.
 
